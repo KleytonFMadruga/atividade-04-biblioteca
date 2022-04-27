ac31459860999d02cea95f85b0b2b88831c8b247
KleytonFMadruga <kleyton.fernandes@dcx.ufpb.br>
2022-04-27 16:41:56 -0300

    Initial commit

---
+++ src/main/java/br/ufpb/dcx/aps/atividades/atv04/atividade04biblioteca/App.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.aps.atividades.atv04.atividade04biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade04BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade04BibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufpb/dcx/aps/atividades/atv04/atividade04biblioteca/Atividade04BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.aps.atividades.atv04.atividade04biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade04BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
